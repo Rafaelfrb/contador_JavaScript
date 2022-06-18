1d5c192c90f49b6373532ced85ba72650715a2ac
Rafael Freitas <rafael.frb@hotmail.com>
2022-06-18 00:51:19 -0300

    Added backward counting and impossibility of choosing Passo = 0

--- script.js
+++ script.js
@@ -5,14 +5,25 @@ function printNumbers(){
     let result = document.getElementById("result");
 
     if (startNumber.value.length == 0 || endNumber.value.length == 0 || incrementNumber.value.length == 0) {
+        result.innerHTML = "Impossível contar!";
         window.alert('[ERRO!] Faltam parâmetros.');
     }else{
-        result.innerHTML = 'Contando... '
+        result.innerHTML = 'Contando... </br>'
         let s = Number(startNumber.value);
         let e = Number(endNumber.value);
         let i = Number(incrementNumber.value);
-        for(let c = s; c <= e; c = c + i){
-            result.innerHTML += `\u{1F449} ${c} `
+        if (i <= 0){
+            window.alert('Passo inválido! Considerando Passo 1.');
+            i = 1;
+        }
+        if(s < e) { //contagem crescente
+            for(let c = s; c <= e; c = c + i){
+                result.innerHTML += `\u{1F449} ${c} `
+            }
+        } else {
+            for(let c = s; c >= e; c -= i ){ //contagem decrescente
+                result.innerHTML += ` ${c} \u{1F449}`
+            }
         }
         result.innerHTML += `\u{1F6A9}`
     }
