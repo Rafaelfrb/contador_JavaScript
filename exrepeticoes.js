0610065e201976208d8adfd9c4d83452bdbe68da
Rafael Freitas <rafael.frb@hotmail.com>
2022-07-09 23:52:15 -0300

    Adding exercises

---
+++ exrepeticoes.js
@@ -0,0 +1,14 @@
+
+console.log('Começou...')
+var c = 1;
+
+while (c<=4){
+    console.log(c);
+    c++;
+};
+
+console.log('Acabamos! Ufa!')
+//faz o mesmo que o código acima
+/*for (var c=1; c<=4 ; c++) {
+    console.log(c);
+}*/
